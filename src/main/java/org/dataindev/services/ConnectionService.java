8c2c6a9cbd8e5837c595ff3db85dde871cd3a036
Benjamín Aráoz <admin@dataindev.com>
2023-08-16 13:22:53 -0300

    Added views/images

--- src/main/java/org/dataindev/services/ConnectionService.java
+++ src/main/java/org/dataindev/services/ConnectionService.java
@@ -4,24 +4,30 @@ import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public class ConnectionService {
+
+    //----------------------------------------------------------------
+    //////////////////////////////// SETTINGS ////////////////////////
     private final String userDB = "root";
     private final String passwordDB = "ethernet";
     private final String nameDB = "tienda";
     private final int portDB = 3306;
     private final String jdbcUrl = "jdbc:mysql://ns1.dataindev.com:"+portDB+"/"+nameDB+"?useSSL=false&serverTimezone=UTC";
 
+    //----------------------------------------------------------------
     private final DataSource dataSource;
     Connection con ;
 
-    public ConnectionService(){
+    public ConnectionService()  {
         var comboPooledDataSource =new ComboPooledDataSource();
 
         comboPooledDataSource.setJdbcUrl(jdbcUrl);
         comboPooledDataSource.setUser(userDB);
         comboPooledDataSource.setPassword(passwordDB);
         comboPooledDataSource.setMaxPoolSize(10);
+
         this.dataSource = comboPooledDataSource;
     }
 
@@ -30,6 +36,7 @@ public class ConnectionService {
             return this.dataSource.getConnection();
         }
         catch(Exception e){
+            e.printStackTrace();
             throw new RuntimeException(e);
         }
     }
